73dd45c7527b3b1181d6898691ad2e80a13a8250
Zoltan <zdebre@gmail.com>
2013-03-29 23:36:26 +0000

    ready and debug on

--- lib/graphed/console.rb
+++ lib/graphed/console.rb
@@ -1,4 +1,5 @@
 require 'readline'
+require 'pp'
 
 module Graphed
   class Looper
@@ -6,8 +7,22 @@ module Graphed
     def self.execute_command(command, image)
 
       case command
-        when /^I (\d) (\d)/
+        when /^I (\d+) (\d+)/
           image = ImageEditor.I($1.to_i, $2.to_i)
+        when /^C/
+          image = ImageEditor.I(image[0].length, image.length)
+        when /^L (\d+) (\d+) (.+$)/
+          image.L($1.to_i, $2.to_i, $3)
+        when /^V (\d+) (\d+) (\d+) (.+$)/
+          image.V($1.to_i, $2.to_i, $3.to_i, $4)
+        when /^H (\d+) (\d+) (\d+) (.+$)/
+          image.H($1.to_i, $2.to_i, $3.to_i, $4)
+        when /^F (\d+) (\d+) (.+$)/
+          image.F($1.to_i, $2.to_i, $3)
+        when /^S/
+          image.S
+        when /^help/
+          help
         else
           puts "No match is found"
       end
@@ -16,10 +31,7 @@ module Graphed
 
     end
 
-
-
-    def self.main
-
+    def self.help
       print <<-EOF
 This program simulates a simple interactive graphical editor.
 
@@ -29,20 +41,38 @@ C           Clears the table, setting all pixels to white (O).
 L X Y C     Colours the pixel (X,Y) with colour C.
 V X Y1 Y2 C Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
 H X1 X2 Y C Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
-F X Y C     Fill the region R with the colur C. R is defined as: Pixel (X,Y) belongs to R. Any other pixel which is the same colour as (X,Y) and shares a common side with any pixel in R also belongs to this region.
+F X Y C     Fill the region R with the colour C. R is defined as: Pixel (X,Y) belongs to R. Any other pixel which is the same colour as (X,Y) and shares a common side with any pixel in R also belongs to this region.
 S           Show the contents of the current image
 X           Terminate the session
+help        Show this help
 
       EOF
+    end
 
+    def self.automata(image)
+      image = execute_command('I 15 10', image)
+      image = execute_command('V 3 3 9 x', image)
+      image = execute_command('V 7 3 9 x', image)
+      image = execute_command('H 3 7 3 x', image)
+      image = execute_command('H 3 7 7 x', image)
+      image = execute_command('S', image)
+    end
+
+    def self.main
+
+
+      help
 
       image = []
 
+      image = automata(image)
+
       loop do
-        line = Readline::readline('> ')
+        line = Readline::readline('GraphEd> ')
         exit if line.nil? || line == 'X'
         Readline::HISTORY.push(line)
-        image = self.execute_command(line, image)
+
+        image = execute_command(line, image)
       end
 
     end
--- lib/graphed/imageeditor.rb
+++ lib/graphed/imageeditor.rb
@@ -1,18 +1,66 @@
 module Graphed
   class ImageEditor < Array
 
-    def initialize(m, n, default_color = 'O')
-       super([Array.new(m, default_color)]*n) #Create an M * N 2 dimensional matrix with 'white'
-       @cols = m
-       @rows = n
+
+    def initialize(m, n,  default_color = 'O')
+
+      super(n) {Array.new(m){default_color}} #Create an M * N 2 dimensional matrix with 'white'
+      @cols = m
+      @rows = n
+
+    end
+
+    def set(x, y, color)
+      self[y-1][x-1] = color
+    end
+
+    def get(x, y)
+      self[y-1][x-1]
+    end
+
+    def vertical(x, y1, y2, color)
+      (y1..y2).each{|y| set(x, y, color)}
+    end
+
+    def horizontal(x1, x2, y, color)
+      (x1..x2).each{|x| set(x, y, color)}
+    end
+
+    #This algorithm from Ruby Forum: http://www.ruby-forum.com/topic/184567
+    def flood_fill(x, y, target_color, replacement_color )
+      return unless x>0 && y>0 && x<=@cols && y<=@rows
+      puts "x=#{x}, y=#{y}, target_color=#{target_color}, replacement_color=#{replacement_color}, get=#{get(x,y)}"
+      show; sleep(0.2)
+
+      return if get(x,y) != target_color
+      return if get(x,y) == replacement_color
+
+      set(x, y, replacement_color)
+      flood_fill(x+1, y, target_color, replacement_color)
+      flood_fill(x-1, y, target_color, replacement_color)
+      flood_fill(x, y+1, target_color, replacement_color)
+      flood_fill(x, y-1, target_color, replacement_color)
+    end
+
+    def fill(x,y, color)
+      flood_fill(x, y, get(x,y), color)
+    end
+
+    def show
+      each {|line| puts line.join}
     end
 
     alias :I :initialize
+    alias :L :set
+    alias :V :vertical
+    alias :H :horizontal
+    alias :F :fill
+    alias :S :show
 
-  public
 
-    def self.I(m,n)
-      ImageEditor.new(m,n)
+    def self.I(m, n)
+       ImageEditor.new(m, n)
     end
+
   end
 end
